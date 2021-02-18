9e32cc7e1541570b15787c6c37b67a4bb289fe6f
InduLekha98 <rindulekha09@gmail.com>
2021-02-19 02:20:07 +0530

    Added:Ability to edit existing contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,45 +1,45 @@
 import java.util.Scanner;
-public class AddressBookMain {
+public class AddressBookMain{
     Scanner s = new Scanner(System.in);
     class Entry{
-        private String FIrst_NAME;
+       	private String FIrst_NAME;
 	private String LAST_NAME;
 	private String ADDRESS;
 	private String PHONE_NO;
 	private String ZIP_CODE;
 	private String EMAIL;
 	Entry(String FIrst_NAME, String LAST_NAME, String ADDRESS, String PHONE_NO, String ZIP_CODE, String EMAIL){
-            this.FIrst_NAME = FIrst_NAME;
+	    this.FIrst_NAME = FIrst_NAME;
 	    this.LAST_NAME = LAST_NAME;
 	    this.ADDRESS = ADDRESS;
 	    this.PHONE_NO = PHONE_NO;
-            this.ZIP_CODE = ZIP_CODE;
+	    this.ZIP_CODE = ZIP_CODE;
 	    this.EMAIL = EMAIL;
         }
         Entry(){
-        	  FIrst_NAME = "";
-        	  LAST_NAME = "";
-        	  ADDRESS = "";
-        	  PHONE_NO = "";
-        	  ZIP_CODE = "";
-        	  EMAIL = "";
+           FIrst_NAME = "";
+           LAST_NAME = "";
+           ADDRESS = "";
+       	   PHONE_NO = "";
+       	   ZIP_CODE = "";
+       	   EMAIL = "";
 	}
 	public void readEntry(){
 	    System.out.println("First Name:"+FIrst_NAME );
-	    System.out.println("Last Name:"+LAST_NAME );
+       	    System.out.println("Last Name:"+LAST_NAME );
 	    System.out.println("Address:"+ADDRESS );
 	    System.out.println("Phone_no:"+PHONE_NO );
-            System.out.println("Zip_code:"+ZIP_CODE );
-            System.out.println("Email:"+EMAIL );
+	    System.out.println("Zip_code:"+ZIP_CODE );
+	    System.out.println("Email:"+EMAIL );
 	}
 	}
-          private int entries = 0;
-          Entry[] contents;
+        private int entries = 0;
+        Entry[] contents;
 
         public void initEntries(int e){
-          contents = new Entry[e];
+            contents = new Entry[e];
         for (int i = 0;i<contents.length;i++){
-          contents[i] = new Entry();
+            contents[i] = new Entry();
         }
     }
 
@@ -55,6 +55,16 @@ public class AddressBookMain {
         else
             System.out.println("Error: book is full");
         }
+
+	public void edit(String FIRST_NAME, String LAST_NAME, String ADDRESS, String PHONE_NO,
+    	    String ZIP_CODE, String EMAIL, int selection){
+            contents[selection].FIRST_NAME = FIRST_NAME;
+            contents[selection].LAST_NAME = LAST_NAME;
+            contents[selection].ADDRESS = ADDRESS;
+            contents[selection].PHONE_NO = PHONE_NO;
+            contents[selection].ZIP_CODE = ZIP_CODE;
+            contents[selection].EMAIL = EMAIL;
+       }
 
         public static void main(String[] args)
         {
@@ -93,9 +103,9 @@ public class AddressBookMain {
                         System.out.println("You must create at least 1 Entry.");
                     }
             }
-              boolean done = false;
-              int selectedBook = 0;
-              int selection;
+            boolean done = false;
+            int selectedBook = 0;
+            int selection;
             while (done==false){
                 System.out.println("Book "+selectedBook+" is currently selected.");
 
@@ -126,7 +136,23 @@ public class AddressBookMain {
                     EMAIL = s.next();
                     library[selectedBook].add(FIRST_NAME, LAST_NAME, ADDRESS, PHONE_NO, ZIP_CODE, EMAIL);
                     break;
-                case 2:
+		case 2:
+                    int whichEntry = s.nextInt();
+                    System.out.print("First name? ");
+                    FIRST_NAME = s.next();
+                    System.out.print("Last name? ");
+                    LAST_NAME = s.next();
+                    System.out.print("Address? ");
+                    ADDRESS = s.next();
+                    System.out.print("Email? ");
+                    PHONE_NO = s.next();
+                    System.out.print("Email? ");
+                    ZIP_CODE = s.next();
+                    System.out.print("Email? ");
+                    EMAIL = s.next();
+                    library[selectedBook].edit(FIRST_NAME, LAST_NAME, ADDRESS, PHONE_NO, ZIP_CODE, EMAIL, whichEntry);
+                    break;
+                case 3:
                     done = true;
                     break;
                 default:
